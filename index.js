d92ef27bca138d14fba76f6ac229b09f5c2da5d4
Certil Remy <certil@noukod.com>
2019-02-08 19:19:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function Driver(name, age, experience)
  this.age = age;
  this.experience = experience;
 }
+
+
+function Scooter(year, color, model)
+{
+ this.year = year;
+ this.color = color;
+ this.model = model;
+}
